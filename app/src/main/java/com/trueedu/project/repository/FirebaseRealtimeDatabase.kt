814118c81197a227299dd2e82776a59fc82ca5ce
babo.neo <seirion@mensakorea.org>
2024-10-07 18:52:00 +0900

    firebase 데이터 기반 강제 업데이트 설정

---
+++ app/src/main/java/com/trueedu/project/repository/FirebaseRealtimeDatabase.kt
@@ -0,0 +1,70 @@
+package com.trueedu.project.repository
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.trueedu.project.BuildConfig
+import com.trueedu.project.repository.local.Local
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FirebaseRealtimeDatabase @Inject constructor(
+    private val local: Local,
+) {
+    companion object {
+        private val TAG = FirebaseRealtimeDatabase::class.java.simpleName
+    }
+
+    // Firebase Realtime Database 인스턴스 가져오기
+    private val database = FirebaseDatabase.getInstance()
+    private val configRef = database.getReference("app_config")
+    private val userId = FirebaseAuth.getInstance().currentUser?.uid
+    init {
+        configRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+
+    suspend fun needForceUpdate(): Boolean {
+        try {
+            if (userId == null) {
+            }
+            val currentVersion = BuildConfig.VERSION_NAME
+            val snapshot = configRef.get().await()
+            val minVersion = snapshot.child("minVersion").getValue(String::class.java)
+
+            return minVersion != null && compareVersions(currentVersion, minVersion) < 0
+        } catch (e: Exception) {
+            // 오류 처리
+            Log.e(TAG, "Error checking for update", e)
+            return false
+        }
+    }
+
+    // 버전 비교 함수 (숫자로 변환하여 비교)
+    private fun compareVersions(version1: String, version2: String): Int {
+        val v1Parts = version1.split(".")
+        val v2Parts = version2.split(".")
+
+        val maxLength = maxOf(v1Parts.size, v2Parts.size)
+        repeat(maxLength) {
+            val v1Part = v1Parts.getOrNull(it)?.toIntOrNull() ?: 0
+            val v2Part = v2Parts.getOrNull(it)?.toIntOrNull() ?: 0
+
+            if (v1Part != v2Part) {
+                return v1Part - v2Part
+            }
+        }
+
+        return 0
+    }
+}
---
+++ app/src/main/java/com/trueedu/project/ui/views/home/ForceUpdateViews.kt
@@ -0,0 +1,73 @@
+package com.trueedu.project.ui.views.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.trueedu.project.ui.common.BasicText
+import com.trueedu.project.ui.common.Margin
+
+@Preview(showBackground = true)
+@Composable
+fun ForceUpdateView(
+    onClick: () -> Unit = {},
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = MaterialTheme.colorScheme.background
+                    .copy(alpha = 0.9f)
+            )
+    ) {
+        Column(
+            horizontalAlignment = Alignment.End,
+            modifier = Modifier
+                .padding(horizontal = 32.dp)
+                .background(
+                    shape = RoundedCornerShape(24.dp),
+                    color = MaterialTheme.colorScheme.background
+                )
+                .padding(16.dp)
+        ) {
+            BasicText(
+                s = "앱을 최신 버전으로 업데이트 해 주세요",
+                fontSize = 14,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.W600,
+            )
+            Margin(4)
+            TextButton(
+                onClick = onClick,
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonColors(
+                    containerColor = MaterialTheme.colorScheme.errorContainer,
+                    contentColor = MaterialTheme.colorScheme.error,
+                    disabledContainerColor = MaterialTheme.colorScheme.errorContainer
+                        .copy(alpha = 0.38f),
+                    disabledContentColor = MaterialTheme.colorScheme.errorContainer
+                        .copy(alpha = 0.38f),
+                )
+            ) {
+                BasicText(
+                    s = "업데이트 하기",
+                    fontSize = 14,
+                    color = MaterialTheme.colorScheme.error,
+                    fontWeight = FontWeight.W600,
+                )
+            }
+        }
+    }
+}
