32865d8d140a21b3c59f2cda2e14ce3853d8fd98
babo.neo <seirion@mensakorea.org>
2024-10-10 16:11:48 +0900

    코스닥 종목 속성 추가

--- app/src/main/java/com/trueedu/project/ui/views/StockDetailFragment.kt
+++ app/src/main/java/com/trueedu/project/ui/views/StockDetailFragment.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentManager
 import com.trueedu.project.data.StockPool
 import com.trueedu.project.model.dto.StockInfo
+import com.trueedu.project.model.dto.StockInfoKosdaq
 import com.trueedu.project.model.dto.StockInfoKospi
 import com.trueedu.project.ui.BaseFragment
 import com.trueedu.project.ui.common.BackTitleTopBar
@@ -60,7 +61,20 @@ class StockDetailFragment: BaseFragment() {
                 "전일거래량" to stockInfo.prevVolume(),
             )
         } else {
-            // TODO
+            val stockInfo = stockInfo as StockInfoKosdaq
+            infoList.value = listOf(
+                "거래정지" to stockInfo.halt(),
+                "관리종목" to stockInfo.designated(),
+                "상장일자" to stockInfo.listingDate(),
+                "상장주수" to stockInfo.listingShares(),
+                "공매도과열" to stockInfo.shortSellingOverheating(),
+                "이상급등" to stockInfo.unusualPriceSurge(),
+                "매출액" to stockInfo.sales(),
+                "영업이익" to stockInfo.operatingProfit(),
+                "시가총액" to stockInfo.marketCap(),
+                "기준가" to stockInfo.prevPrice(),
+                "전일거래량" to stockInfo.prevVolume(),
+            )
         }
     }
 
