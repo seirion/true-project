e3a3fad4df8c83341526032720e99e80fec02cff
babo.neo <seirion@mensakorea.org>
2024-10-31 23:06:21 +0900

    스팩 종목 화면

--- app/src/main/java/com/trueedu/project/ui/views/StockDetailViewModel.kt
+++ app/src/main/java/com/trueedu/project/ui/views/StockDetailViewModel.kt
@@ -6,8 +6,6 @@ import androidx.lifecycle.viewModelScope
 import com.trueedu.project.data.RealPriceManager
 import com.trueedu.project.data.StockPool
 import com.trueedu.project.model.dto.StockInfo
-import com.trueedu.project.model.dto.StockInfoKosdaq
-import com.trueedu.project.model.dto.StockInfoKospi
 import com.trueedu.project.model.dto.price.PriceResponse
 import com.trueedu.project.repository.remote.PriceRemote
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -52,36 +50,18 @@ class StockDetailViewModel @Inject constructor(
     }
 
     private fun initInfoList() {
-        if (stockInfo is StockInfoKospi) {
-            val stockInfo = stockInfo as StockInfoKospi
-            infoList.value = listOf(
-                "거래정지" to stockInfo.halt(),
-                "관리종목" to stockInfo.designated(),
-                "상장일자" to stockInfo.listingDate(),
-                "상장주수" to stockInfo.listingShares(),
-                "공매도과열" to stockInfo.shortSellingOverheating(),
-                "이상급등" to stockInfo.unusualPriceSurge(),
-                "매출액" to stockInfo.sales(),
-                "영업이익" to stockInfo.operatingProfit(),
-                "시가총액" to stockInfo.marketCap(),
-                "기준가" to stockInfo.prevPrice(),
-                "전일거래량" to stockInfo.prevVolume(),
-            )
-        } else {
-            val stockInfo = stockInfo as StockInfoKosdaq
-            infoList.value = listOf(
-                "거래정지" to stockInfo.halt(),
-                "관리종목" to stockInfo.designated(),
-                "상장일자" to stockInfo.listingDate(),
-                "상장주수" to stockInfo.listingShares(),
-                "공매도과열" to stockInfo.shortSellingOverheating(),
-                "이상급등" to stockInfo.unusualPriceSurge(),
-                "매출액" to stockInfo.sales(),
-                "영업이익" to stockInfo.operatingProfit(),
-                "시가총액" to stockInfo.marketCap(),
-                "기준가" to stockInfo.prevPrice(),
-                "전일거래량" to stockInfo.prevVolume(),
-            )
-        }
+        infoList.value = listOf(
+            "거래정지" to stockInfo.halt().toString(),
+            "관리종목" to stockInfo.designated().toString(),
+            "상장일자" to stockInfo.listingDate(),
+            "상장주수" to stockInfo.listingShares(),
+            "공매도과열" to stockInfo.shortSellingOverheating().toString(),
+            "이상급등" to stockInfo.unusualPriceSurge().toString(),
+            "매출액" to stockInfo.sales(),
+            "영업이익" to stockInfo.operatingProfit(),
+            "시가총액" to stockInfo.marketCap(),
+            "기준가" to stockInfo.prevPrice(),
+            "전일거래량" to stockInfo.prevVolume(),
+        )
     }
 }
