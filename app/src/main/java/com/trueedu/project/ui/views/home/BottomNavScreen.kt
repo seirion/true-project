fcc736e1057166dd47add86ea89b91d5d1c6d16a
babo.neo <seirion@mensakorea.org>
2024-11-05 15:02:53 +0900

    화면 진입 시 이벤트 로깅

--- app/src/main/java/com/trueedu/project/ui/views/home/BottomNavScreen.kt
+++ app/src/main/java/com/trueedu/project/ui/views/home/BottomNavScreen.kt
@@ -3,9 +3,11 @@ package com.trueedu.project.ui.views.home
 import androidx.compose.runtime.Composable
 
 interface BottomNavScreen {
-    fun onCreate()
-    fun onStart() // 화면이 보일 때
-    fun onStop() // 화면이 가려질 때
+    fun onCreate() {}
+    fun onStart() {} // 화면이 보일 때
+    fun onResume() {}
+    fun onPause() {}
+    fun onStop() {} // 화면이 가려질 때
 
     fun screenName(): String {
         val simpleName = this::class.java.simpleName
