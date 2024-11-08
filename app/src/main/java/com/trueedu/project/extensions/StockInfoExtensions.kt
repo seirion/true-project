37937bc53e4304cc950fc68a3e12feca8335bf4b
babo.neo <seirion@mensakorea.org>
2024-11-08 22:45:53 +0900

    dark mode 용 등락 색상 추가

--- app/src/main/java/com/trueedu/project/extensions/StockInfoExtensions.kt
+++ app/src/main/java/com/trueedu/project/extensions/StockInfoExtensions.kt
@@ -1,5 +1,6 @@
 package com.trueedu.project.extensions
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.trueedu.project.model.dto.price.PriceResponse
 import com.trueedu.project.model.ws.RealTimeTrade
@@ -13,18 +14,21 @@ private val rateFormatter = RateFormatter()
 /**
  * return [priceString, textColor]
  */
+@Composable
 fun priceChangeStr(priceInfo: RealTimeTrade?): Pair<String, Color> {
     val priceChange = priceInfo?.delta
     val rate = priceInfo?.rate
     return priceChangeStr(priceChange, rate)
 }
 
+@Composable
 fun priceChangeStr(priceResponse: PriceResponse): Pair<String, Color> {
     val priceChange = priceResponse.output.priceChange.toDouble()
     val rate = priceResponse.output.priceChangeRate.toDouble()
     return priceChangeStr(priceChange, rate)
 }
 
+@Composable
 private fun priceChangeStr(priceChange: Double?, rate: Double?): Pair<String, Color> {
     if (priceChange != null && rate != null) {
         return "${cashFormatter.format(priceChange, true)} (" +
--- app/src/main/java/com/trueedu/project/ui/theme/Color.kt
+++ app/src/main/java/com/trueedu/project/ui/theme/Color.kt
@@ -1,13 +1,35 @@
 package com.trueedu.project.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-// light mode only
 object ChartColor {
-    val up = Color(0xFFFF2D55)
-    val down = Color(0xFF007BFF)
-    val neutral = Color(0xFF68727C)
+    val up: Color
+        @Composable
+        get() = if (isSystemInDarkTheme()) {
+            Color(0xFFFF587A) // Dark mode up color
+        } else {
+            Color(0xFFFF2D55) // Light mode up color
+        }
 
+    val down: Color
+        @Composable
+        get() = if (isSystemInDarkTheme()) {
+            Color(0xFF3398FF) // Dark mode down color
+        } else {
+            Color(0xFF007BFF) // Light mode down color
+        }
+
+    val neutral: Color
+        @Composable
+        get() = if (isSystemInDarkTheme()) {
+            Color(0xFFA8B1BB) // Dark mode down color
+        } else {
+            Color(0xFF68727C) // Light mode down color
+        }
+
+    @Composable
     fun color(v: Double): Color {
         return when {
             v == 0.0 -> neutral
