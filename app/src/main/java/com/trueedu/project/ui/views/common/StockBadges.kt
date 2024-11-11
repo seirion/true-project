63a06e1d46d8b3b3390956c9d95b54a828a6a28b
babo.neo <seirion@mensakorea.org>
2024-11-11 17:26:02 +0900

    보유 배지 추가

--- app/src/main/java/com/trueedu/project/ui/views/common/StockBadges.kt
+++ app/src/main/java/com/trueedu/project/ui/views/common/StockBadges.kt
@@ -2,13 +2,17 @@ package com.trueedu.project.ui.views.common
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import com.trueedu.project.ui.common.TrueText
 
 @Composable
@@ -26,6 +30,31 @@ fun Badge(
             s = s,
             fontSize = 10,
             color = Color.White,
+            lineHeight = 1.0.em,
+        )
+    }
+}
+
+@Composable
+fun RoundedBadge(
+    s: String = "보유",
+    bgColor: Color = Color.Red,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .height(12.dp)
+            .background(
+                color = bgColor,
+                shape = RoundedCornerShape(3.dp)
+            )
+            .padding(horizontal = 2.dp)
+    ) {
+        TrueText(
+            s = s,
+            fontSize = 10,
+            color = Color.White,
+            lineHeight = 1.0.em,
         )
     }
 }
@@ -40,4 +69,10 @@ fun HaltBadge() {
 @Composable
 fun DesignatedBadge() {
     Badge("관", Color(0xFFF57C00))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HoldingBadge() {
+    RoundedBadge("보유", Color(0xFF448A33))
 }
