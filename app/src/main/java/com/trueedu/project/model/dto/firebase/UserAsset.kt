b0eafe5b21da37cb62de3aae8f7f5ced0841d8ef
babo.neo <seirion@mensakorea.org>
2024-11-12 23:49:42 +0900

    UserAsset data class 로 선언

--- app/src/main/java/com/trueedu/project/model/dto/firebase/UserAsset.kt
+++ app/src/main/java/com/trueedu/project/model/dto/firebase/UserAsset.kt
@@ -3,7 +3,7 @@ package com.trueedu.project.model.dto.firebase
 /**
  * firebase database 에 보유 자산을 저장하기 위한 DTO
  */
-class UserAsset(
+data class UserAsset(
     val code: String,
     val nameKr: String,
     val price: Double,
