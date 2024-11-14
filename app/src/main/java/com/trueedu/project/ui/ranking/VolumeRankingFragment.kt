5937a6195e06675ec55583dc8a9539fefdaf3621
babo.neo <seirion@mensakorea.org>
2024-11-14 15:58:27 +0900

    거래량 순위 화면에 거래량 표시

--- app/src/main/java/com/trueedu/project/ui/ranking/VolumeRankingFragment.kt
+++ app/src/main/java/com/trueedu/project/ui/ranking/VolumeRankingFragment.kt
@@ -1,9 +1,12 @@
 package com.trueedu.project.ui.ranking
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -11,6 +14,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentManager
@@ -18,8 +22,9 @@ import androidx.fragment.app.viewModels
 import com.trueedu.project.model.dto.rank.VolumeRankingOutput
 import com.trueedu.project.ui.BaseFragment
 import com.trueedu.project.ui.common.BackTitleTopBar
-import com.trueedu.project.ui.common.TrueText
 import com.trueedu.project.ui.common.LoadingView
+import com.trueedu.project.ui.common.TrueText
+import com.trueedu.project.utils.formatter.cashFormatter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -75,12 +80,25 @@ private fun StockList(
             .padding(horizontal = 8.dp)
     ) {
         itemsIndexed(list, key = { _, item -> item.code }) { index, item ->
-            TrueText(
-                s = "${item.rank}. ${item.nameKr} (${item.code})",
-                fontSize = 14,
-                color = MaterialTheme.colorScheme.secondary,
-                modifier = Modifier.padding(vertical = 4.dp)
-            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(vertical = 4.dp)
+            ) {
+                TrueText(
+                    s = "${item.rank}. ${item.nameKr}",
+                    fontSize = 14,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.weight(1f)
+                )
+                val volumeString = cashFormatter.format(item.volume.toDouble())
+                TrueText(
+                    s = volumeString,
+                    fontSize = 13,
+                    color = MaterialTheme.colorScheme.secondary,
+                )
+            }
         }
     }
 }
