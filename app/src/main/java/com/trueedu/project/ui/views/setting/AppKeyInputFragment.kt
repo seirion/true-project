218f09e73e1a4e7c164d3337b618a31ccffe76b8
babo.neo <seirion@mensakorea.org>
2024-11-26 16:34:34 +0900

    이미지 줌 가능하게 하기

--- app/src/main/java/com/trueedu/project/ui/views/setting/AppKeyInputFragment.kt
+++ app/src/main/java/com/trueedu/project/ui/views/setting/AppKeyInputFragment.kt
@@ -6,7 +6,8 @@ import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,10 +27,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
@@ -306,17 +312,11 @@ fun FullScreenImage(
     onClose: () -> Unit // Callback for closing the image
 ) {
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .padding(top = 32.dp)
     ) {
-        Image(
-            painter = painterResource(id = image),
-            contentDescription = "Appkey Issuance Guide",
-            modifier = Modifier
-                .fillMaxWidth()
-                .horizontalScroll(rememberScrollState()), // Enable horizontal scrolling
-            contentScale = ContentScale.FillWidth // Fit width
-        )
+        ZoomableImage(image = painterResource(id = image))
         IconButton(
             onClick = onClose,
             modifier = Modifier
@@ -325,5 +325,34 @@ fun FullScreenImage(
         ) {
             Icon(Icons.Filled.Close, contentDescription = "close")
         }
+    }
+}
+
+@Composable
+fun ZoomableImage(image: Painter) {
+    var scale by remember { mutableStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
+        scale = (scale * zoomChange).coerceAtLeast(1f)
+        offset += panChange
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .transformable(state = state)
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                    translationX = offset.x
+                    translationY = offset.y
+                }
+                .fillMaxSize()
+        )
     }
 }
