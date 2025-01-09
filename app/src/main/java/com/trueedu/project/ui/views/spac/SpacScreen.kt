b938dd5d34cbbcd2ecfd78257f2e3b59a967bcad
babo.neo <seirion@mensakorea.org>
2025-01-09 21:24:13 +0900

    정렬 버튼 변경
    정렬 방식을 텍스트로 표시

--- app/src/main/java/com/trueedu/project/ui/views/spac/SpacScreen.kt
+++ app/src/main/java/com/trueedu/project/ui/views/spac/SpacScreen.kt
@@ -2,15 +2,22 @@ package com.trueedu.project.ui.views.spac
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ChevronLeft
 import androidx.compose.material.icons.outlined.ViewList
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Scaffold
@@ -19,8 +26,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentManager
 import com.trueedu.project.MainViewModel
 import com.trueedu.project.analytics.TrueAnalytics
@@ -31,6 +41,7 @@ import com.trueedu.project.ui.ads.NativeAdView
 import com.trueedu.project.ui.common.BottomSelectionFragment
 import com.trueedu.project.ui.common.CustomTopBar
 import com.trueedu.project.ui.common.LoadingView
+import com.trueedu.project.ui.common.TouchIcon24
 import com.trueedu.project.ui.common.TouchIcon32
 import com.trueedu.project.ui.common.TrueText
 import com.trueedu.project.ui.views.StockDetailFragment
@@ -58,7 +69,7 @@ class SpacScreen(
     override fun Draw() {
         Scaffold(
             topBar = {
-                SpacScreenTopBar(::onSortOption)
+                SpacScreenTopBar(vm.sort.value, ::onSortOption)
             },
             bottomBar = {
                 if (remoteConfig.adVisible.value && admobManager.nativeAd.value != null) {
@@ -162,6 +173,7 @@ class SpacScreen(
 @Preview(showBackground = true)
 @Composable
 private fun SpacScreenTopBar(
+    sortType: SpacSort = SpacSort.ISSUE_DATE,
     onSortOption: () -> Unit = {},
 ) {
     CustomTopBar(
@@ -170,13 +182,34 @@ private fun SpacScreenTopBar(
         },
         titleView = {
             TrueText(
-                s = "스팩 검색",
+                s = "스팩",
                 fontSize = 20,
                 color = MaterialTheme.colorScheme.primary
             )
         },
         actionsView = {
-            TouchIcon32(icon = Icons.Outlined.ViewList, onClick = onSortOption)
+            Row(
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(end = 4.dp)
+                    .clip(shape = RoundedCornerShape(24.dp))
+                    .clickable { onSortOption() }
+                    .padding(16.dp, 10.dp, 4.dp, 10.dp)
+            ) {
+                TrueText(
+                    s = sortType.title,
+                    fontSize = 16,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    tint = MaterialTheme.colorScheme.primary,
+                    contentDescription = "sort-select"
+                )
+            }
+            TouchIcon24(icon = Icons.Outlined.ViewList, onClick = {})
         }
     )
 }
