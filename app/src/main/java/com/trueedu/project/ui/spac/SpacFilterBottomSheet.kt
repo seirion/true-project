1976885ef7c9a3e75fb3e5572c0e28611f9c6ef4
babo.neo <seirion@mensakorea.org>
2025-01-17 19:13:57 +0900

    스팩 종목 필터

--- app/src/main/java/com/trueedu/project/ui/spac/SpacFilterBottomSheet.kt
+++ app/src/main/java/com/trueedu/project/ui/spac/SpacFilterBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.trueedu.project.ui.spac
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,42 +10,62 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.trueedu.project.R
+import com.trueedu.project.analytics.TrueAnalytics
+import com.trueedu.project.data.ScreenControl
 import com.trueedu.project.data.spac.SpacManager
 import com.trueedu.project.repository.local.Local
-import com.trueedu.project.ui.BaseFragment
 import com.trueedu.project.ui.common.RoundedTopColumn
 import com.trueedu.project.ui.common.TrueText
 import com.trueedu.project.ui.theme.TrueProjectTheme
 import com.trueedu.project.ui.views.setting.OnOffSetting
+import com.trueedu.project.ui.views.spac.SpacFilter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SpacFilterBottomSheet: BaseFragment() {
+class SpacFilterBottomSheet: BottomSheetDialogFragment() {
     companion object {
         fun show(
+            initialValue: SpacFilter,
             fragmentManager: FragmentManager,
+            onCompleted: (SpacFilter) -> Unit
         ): SpacFilterBottomSheet {
             return SpacFilterBottomSheet().also {
+                it.spacFilter.value = initialValue
+                it.onCompleted = onCompleted
                 it.show(fragmentManager, "spac-filter-sheet")
             }
         }
     }
 
     @Inject
+    lateinit var screen: ScreenControl
+    @Inject
     lateinit var local: Local
     @Inject
+    lateinit var trueAnalytics: TrueAnalytics
+    @Inject
     lateinit var spacManager: SpacManager
+
+    var spacFilter = mutableStateOf(SpacFilter())
+    var onCompleted: (SpacFilter) -> Unit = {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(STYLE_NORMAL, R.style.PopupDialog)
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        onCompleted(spacFilter.value)
     }
 
     override fun onCreateView(
@@ -83,6 +104,17 @@ class SpacFilterBottomSheet: BaseFragment() {
                 spacManager.spacAnnualProfitMode.value,
                 ::setSpacAnnualProfit
             )
+            OnOffSetting(
+                "1년 미만 종목만 보기",
+                spacFilter.value.listedOverTwoYears,
+                {
+                    trueAnalytics.clickToggleButton(
+                        "${screenName()}__listed_over_two_years__click",
+                        !it
+                    )
+                    spacFilter.value = spacFilter.value.copy(listedOverTwoYears = it)
+                }
+            )
         }
     }
 
@@ -90,6 +122,8 @@ class SpacFilterBottomSheet: BaseFragment() {
         trueAnalytics.clickToggleButton("${screenName()}__spac_annual_profit__click", !on)
         spacManager.setSpacAnnualProfit(on)
     }
+
+    private fun screenName() = "spac_filter"
 }
 
 @Composable
---
+++ app/src/main/java/com/trueedu/project/ui/views/spac/SpacFilter.kt
@@ -0,0 +1,5 @@
+package com.trueedu.project.ui.views.spac
+
+data class SpacFilter(
+    val listedOverTwoYears: Boolean = false, // 3년 차 종목
+)
