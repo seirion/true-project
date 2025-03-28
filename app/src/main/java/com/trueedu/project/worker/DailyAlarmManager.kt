a8d359cfb7342f44485dfe6250dd02ebaf2a24f4
babo.neo <seirion@mensakorea.org>
2025-03-28 16:27:01 +0900

    AlarmManager 기능 제거

--- app/src/main/java/com/trueedu/project/worker/DailyAlarmManager.kt
+++ app/src/main/java/com/trueedu/project/worker/DailyAlarmManager.kt
@@ -1,16 +1,10 @@
 package com.trueedu.project.worker
 
 import android.app.AlarmManager
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
-import android.util.Log
-import com.trueedu.project.BuildConfig
-import com.trueedu.project.broadcast.DailyOrderTaskReceiver
-import java.time.ZoneId
-import java.time.ZonedDateTime
 
 class DailyAlarmManager(private val context: Context) {
 
@@ -22,47 +16,6 @@ class DailyAlarmManager(private val context: Context) {
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
     fun scheduleDailyTask() {
-        Log.d(TAG, "scheduleDailyTask() : ${isAlarmSet()}")
-
-        if (!checkAlarmPermission()) {
-            Log.d(TAG, "Not Alarm Permission")
-            return
-        }
-
-        if (isAlarmSet()) cancelAlarm()
-        if (!BuildConfig.DEBUG) {
-            // 일단 기능 막음
-            return
-        }
-
-        // 한국 시간으로 오전 8시 50분 설정
-        val now = ZonedDateTime.now(ZoneId.of("Asia/Seoul"))
-
-        var scheduledTime = now.withHour(8)
-            .withMinute(50)
-            .withSecond(0)
-
-        // 현재 시간이 이미 8:50을 지났다면 다음날로 설정
-        if (scheduledTime.isBefore(now)) {
-            scheduledTime = scheduledTime.plusDays(1)
-        }
-        Log.d(TAG, "next schedule: $scheduledTime")
-
-        // AlarmManager 에서 사용하기 위해 밀리초로 변환
-        val triggerTimeMillis = scheduledTime.toInstant().toEpochMilli()
-
-        val intent = Intent(context, DailyOrderTaskReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            context,
-            ALARM_REQUEST_CODE,
-            intent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-
-        alarmManager.setAlarmClock(
-            AlarmManager.AlarmClockInfo(triggerTimeMillis, pendingIntent),
-            pendingIntent
-        )
     }
 
     private fun checkAlarmPermission(): Boolean {
@@ -75,33 +28,5 @@ class DailyAlarmManager(private val context: Context) {
             }
         }
         return true
-    }
-
-    private fun isAlarmSet(): Boolean {
-        val intent = Intent(context, DailyOrderTaskReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            context,
-            ALARM_REQUEST_CODE,
-            intent,
-            PendingIntent.FLAG_NO_CREATE or PendingIntent.FLAG_IMMUTABLE // 이미 존재하는지만 확인
-        )
-        return pendingIntent != null
-    }
-
-    // 알람 취소 메소드 추가
-    private fun cancelAlarm() {
-        val intent = Intent(context, DailyOrderTaskReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            context,
-            ALARM_REQUEST_CODE,
-            intent,
-            PendingIntent.FLAG_NO_CREATE or PendingIntent.FLAG_IMMUTABLE
-        )
-
-        pendingIntent?.let {
-            alarmManager.cancel(it)
-            it.cancel()
-            Log.d("DailyAlarmManager", "Alarm cancelled")
-        }
     }
 }
